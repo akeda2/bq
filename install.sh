9292205cb1c0746f4c2eff9671cd449b46f666fb
akeda <akeda2@gmail.com>
2021-10-04 15:53:38 +0200

    Moved check for inotifywait to end of installer

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 for a in *.sh; do
 	f="${a%.*}"
 	[[ $a == install.sh ]] || sudo ln -sf $(realpath "$a") /usr/local/bin/"$f"
-	[ $(command -v inotifywait) ] || sudo apt -y install inotify-tools
 done
+
+[ $(command -v inotifywait) ] || sudo apt -y install inotify-tools
