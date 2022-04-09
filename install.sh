29fcdbdc2566d9e7c07118a4c69daac50200dba7
akeda <akeda2@gmail.com>
2022-04-10 00:20:47 +0200

    basename install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 for a in ./*.sh; do
 	f="${a%.*}"
-	[[ $a == install.sh ]] || sudo ln -sf $(realpath "$a") /usr/local/bin/"$f"
+	[[ $(basename $a) == install.sh ]] || sudo ln -sf $(realpath "$a") /usr/local/bin/"$f"
 done
 
 [ $(command -v inotifywait) ] || sudo apt -y install inotify-tools
