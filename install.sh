155644cb91a2e80a50db59e5a4eae5db14f91288
akeda <akeda2@gmail.com>
2022-11-28 09:44:15 +0100

    installer copy iso symlink

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 for a in ./*.sh; do
 	f="${a%.*}"
-	[[ $(basename $a) == install.sh ]] || sudo ln -sf $(realpath "$a") /usr/local/bin/"$f"
+#	[[ $(basename $a) == install.sh ]] || sudo ln -sf $(realpath "$a") /usr/local/bin/"$f"
+        [[ $(basename $a) == install.sh ]] || sudo cp --remove-destination $(realpath "$a") /usr/local/bin/"$f"
+	[[ -f /usr/local/bin/"$f" ]] && sudo chmod a+x /usr/local/bin/"$f"
+
 done
 
 [ $(command -v inotifywait) ] || sudo apt -y install inotify-tools
