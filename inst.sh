63b5315c36c036dc129386cd5d227abcf9796a8a
akeda <akeda2@gmail.com>
2023-11-07 11:18:17 +0100

    rename

--- inst.sh
+++ inst.sh
@@ -2,8 +2,8 @@
 
 for a in ./*.sh; do
 	f="${a%.*}"
-#	[[ $(basename $a) == install.sh ]] || sudo ln -sf $(realpath "$a") /usr/local/bin/"$f"
-        [[ $(basename $a) == install.sh ]] || sudo cp --remove-destination $(realpath "$a") /usr/local/bin/"$f"
+#	[[ $(basename $a) == inst.sh ]] || sudo ln -sf $(realpath "$a") /usr/local/bin/"$f"
+        [[ $(basename $a) == inst.sh ]] || sudo cp --remove-destination $(realpath "$a") /usr/local/bin/"$f"
 	[[ -f /usr/local/bin/"$f" ]] && sudo chmod a+x /usr/local/bin/"$f"
 
 done
